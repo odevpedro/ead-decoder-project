5c16254250c7dbde18cd068d779abd6af3441e14
odevpedro <pedrosschmidt2@gmail.com>
2025-11-19 08:48:40 -0300

    Validator, restrictions on course creation

---
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -0,0 +1,54 @@
+package com.ead.course.validation;
+
+import com.ead.course.clients.AuthUserClient;
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.enums.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import java.util.UUID;
+
+@Component
+public class CourseValidator implements Validator {
+
+    @Autowired
+    @Qualifier("defaultValidator")
+    private Validator defaultValidator;
+
+    @Autowired
+    private AuthUserClient authUserClient;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return CourseDto.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        CourseDto courseDto = (CourseDto) target;
+        defaultValidator.validate(courseDto, errors);
+        if (!errors.hasErrors()) {
+            validateUserInstructor(courseDto.getUserInstructor(), errors);
+        }
+    }
+
+    private void validateUserInstructor(UUID userInstructor, Errors errors) {
+        ResponseEntity<UserDto> responseUserInstructor;
+        try {
+            responseUserInstructor = authUserClient.getOneUserByid(userInstructor);
+            if (responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT)) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
+            }
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User not found");
+            }
+        }
+    }}
